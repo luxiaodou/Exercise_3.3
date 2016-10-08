08dab5106b0cd72aac9ecb3b090de33a96dbf890
luxiaodou <lusongchn@gmail.com>
2016-10-08 12:25:38 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by luxia on 2016/10/8.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/WordAnalyze.java
@@ -0,0 +1,9 @@
+/**
+ * Created by luxia on 2016/10/8.
+ */
+public class WordAnalyze {
+    char c;
+    char[] token;
+    int num;
+    Symbol symbol;
+}
